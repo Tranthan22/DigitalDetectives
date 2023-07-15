2e560085c1479979d80503c578a36f6668d0b79f
Bùi Hiếu <buihieu2707hp@gmail.com>
2023-07-15 12:56:20 +0700

    Delete Station-Gateway directory

--- Station-Gateway/Backup_data.py
+++
@@ -1,9 +0,0 @@
-def save_data_to_file(data):
-    with open("data.txt", "a") as file:
-        file.write(data + "\n")
-        print("Data saved successfully.")
-
-# Ví dụ về việc sử dụng hàm save_data_to_file:
-data_to_save = "Hello, world!"
-print(type(data_to_save))
-save_data_to_file(data_to_save)
