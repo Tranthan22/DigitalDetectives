83d215bb640d6c165762f036ede5e9d04e6b2b5f
jengpham@gmaim.com <jengpham@gmail.com>
2023-07-21 21:06:59 +0700

    .

--- code_node_sensor/GNU ARM v10.3.1 - Debug/gecko_sdk_4.3.0/platform/service/system/src/sl_system_init.d
+++
@@ -1,6 +0,0 @@
-gecko_sdk_4.3.0/platform/service/system/src/sl_system_init.o: \
- C:/Users/jengp/SimplicityStudio/SDKs/gecko_sdk/platform/service/system/src/sl_system_init.c \
- c:\users\jengp\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
- C:\Users\jengp\SimplicityStudio\v5_workspace\IoT_Challenge\autogen/sl_event_handler.h
-c:\users\jengp\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
-C:\Users\jengp\SimplicityStudio\v5_workspace\IoT_Challenge\autogen/sl_event_handler.h:
