4b3b001e2c18c22a62ce3fce11787ba5e8a80666
Pham Thanh Phong <jengpham@gmail.com>
2023-08-29 10:41:51 +0700

    .

--- Sensor/iadc.c
+++ Sensor/iadc.c
@@ -7,7 +7,6 @@
 
 #include "iadc.h"
 
-
 void iadcInit(void){
 
   IADC_Init_t init = IADC_INIT_DEFAULT;
@@ -28,12 +27,10 @@ void iadcInit(void){
                                                                      0,
                                                                      iadcCfgModeNormal,
                                                                      init.srcClkPrescale);
-
   initSingle.triggerAction = iadcTriggerActionOnce;
   initSingleInput.posInput = iadcPosInputPortAPin5; /*P43*/
   initSingleInput.negInput = iadcNegInputGnd;
   GPIO->ABUSALLOC |= GPIO_ABUSALLOC_AODD0_ADC0;
-
 
   IADC_init(IADC0, &init, &initAllConfigs);
   IADC_initSingle(IADC0, &initSingle, &initSingleInput);
